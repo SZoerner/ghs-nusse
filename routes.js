15ebe2ec190cad9cdcfb6f3275f961a840067754
Sebastian Zoerner <zoerner7@gmail.com>
2016-06-05 12:58:44 +0200

    Swapped history.js for Path.js for client-side routing.

---
+++ routes.js
@@ -0,0 +1,20 @@
+// Here we define our routes to load pages by naming convention.
+// e.g. #/startseite -> startseite.html
+Path.map("#/:route").to(function(){
+  $("#content").load(this.params["route"] + ".html");
+});
+
+
+// default page.
+Path.root("#/startseite");
+
+// The `Path.rescue()` method calls a function when a route is activated that you have not yet defined an action for.
+function notFound(){
+  $("#content").html("404 Not Found");
+}
+Path.rescue(notFound);
+
+$(document).ready(function(){
+    // This line is used to start the PathJS listener.
+    Path.listen();
+});
