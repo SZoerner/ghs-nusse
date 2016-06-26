df7ebc65675cf55f49e03714374e1c9e75f300af
Sebastian Zoerner <zoerner7@gmail.com>
2016-06-26 11:46:27 +0200

    Sticky navbar.

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@
 // e.g. #/startseite -> startseite.html
 Path.map("#/:route").to(function(){
   $("#content").load(this.params["route"] + ".html");
+  // scroll to top when switching pages
+  $('body').scrollTop(0);
 });
 
 
@@ -22,4 +24,20 @@ $(document).ready(function(){
         $('#navbar').find('li').removeClass('active');
         $(this).addClass('active');
     });
+    // sticky navbar
+    var menu = $('.navbar');
+    var content = $('.main');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            menu.addClass('navbar-fixed-top');
+            content.addClass('menu-padding');
+        } else {
+            menu.removeClass('navbar-fixed-top');
+            content.removeClass('menu-padding');
+        }
+      };
+      document.onscroll = scroll;
+
 });
